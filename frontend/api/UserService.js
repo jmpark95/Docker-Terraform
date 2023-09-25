21e842f2806d9b17b638b9866ee036d8abffe581
jmpark95 <jmpark95@live.com>
2023-09-25 13:45:48 +1000

    First commit

---
+++ frontend/api/UserService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const UserService = {
+   getAllUsers: async () => {
+      return await axios.get(`${import.meta.env.VITE_BACKEND_URL}/users`);
+   },
+
+   addUser: async (formInput) => {
+      await axios.post(`${import.meta.env.VITE_BACKEND_URL}/signup`, formInput);
+   },
+};
---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { UserService } from "../api/UserService";
+
+function App() {
+   const [users, setUsers] = useState([]);
+   const [formInput, setFormInput] = useState({
+      name: "",
+      email: "",
+   });
+
+   function handleChange(e) {
+      const { name, value } = e.target;
+      setFormInput((prevFormInput) => ({
+         ...prevFormInput,
+         [name]: value,
+      }));
+   }
+
+   async function handleSubmit(e) {
+      e.preventDefault();
+      await UserService.addUser(formInput);
+   }
+
+   useEffect(() => {
+      async function getAllUsers() {
+         const response = await UserService.getAllUsers();
+         setUsers(response.data);
+      }
+
+      getAllUsers();
+   }, []);
+
+   return (
+      <>
+         {users.map((user) => (
+            <div key={user.id}>
+               <div>name: {user.name}</div>
+               <div>email: {user.email}</div>
+            </div>
+         ))}
+
+         <form onSubmit={handleSubmit}>
+            <input type="text" name="name" value={formInput.name} onChange={handleChange} placeholder="name" />
+            <input type="text" name="email" value={formInput.email} onChange={handleChange} placeholder="email" />
+            <button type="submit">Submit</button>
+         </form>
+      </>
+   );
+}
+
+export default App;
